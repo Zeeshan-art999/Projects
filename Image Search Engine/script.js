c303bed0c3f7a2be8e011d2df1aebfc5583360e7
Zeeshan <baog83600@gmail.com>
2025-12-06 17:55:07 -0800

    HTML,CSS,JS Project

---
+++ Image Search Engine/script.js
@@ -0,0 +1,22 @@
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword="";
+let page=1;
+
+async function searchImages() {
+  keyword=searchBox.value;
+  const url =``
+
+  const response = await fetch(url);
+  const data =await response.json();
+
+  console.log(data);
+}
+searchForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    page=1;
+    searchImages()
+})
